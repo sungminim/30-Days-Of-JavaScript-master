e6ff74c753e49904b034fcc4cf97fa3447ddebc8
sungminim <__so1@hanmail.net>
2021-04-13 17:26:37 +0900

    day2/ing

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -1,1 +1,13 @@
 // this is your main.js script
+
+let isRaining = true
+
+isRaining
+    ? console.log('its raining')
+    : console.log('no raining')
+
+isRaining = false
+
+isRaining
+    ? console.log('its raining')
+    : console.log('no raining')
