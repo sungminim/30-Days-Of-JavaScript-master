48585fd91164b96c3ba1e28ea313ea95d68860c0
sungminim <__so1@hanmail.net>
2021-04-13 14:32:58 +0900

    day2/완료

--- 01_Day_Introduction/01_day_starter/varaible.js
+++ 01_Day_Introduction/01_day_starter/varaible.js
@@ -1,20 +1,11 @@
-// Declaring different variables of different data types
+let fruit = 'apple';
+let numbering = 353;
+let boolen = true;
+let hi = null;
+let hi2 = undefined;
 
-let firstName = 'Asabeneh'    // first name of a person
-let lastName = 'Yetayeh'      // last name of a person
-let country = 'Finland'       // country
-let city = 'Helsinki'         // capital city
-let age = 100                 // age in years
-let isMarried = true
-
-// Declaring variables with number values
-
-const gravity = 9.81         // earth gravity  in m/s2
-const boilingPoint = 100     // water boiling point, temperature in oC
-const PI = 3.14              // geometrical constant
-
-// Variables can also be declaring in one line separated by comma
-
-let name = 'Asabeneh',     //name of a person
-  job = 'teacher',
-  live = 'Finland'
+console.log(typeof fruit)
+console.log(typeof numbering)
+console.log(typeof boolen)
+console.log(typeof hi)
+console.log(typeof hi2)
--- 01_Day_Introduction/variable.js
+++ 01_Day_Introduction/variable.js
@@ -1,17 +1,30 @@
+//변수 선언 : let은 재할당이 가능하므로 바꿀 수 있을 여지 있는 것
 // Declaring different variables of different data types
-let firstName = 'Asabeneh' // first name of a person
-let lastName = 'Yetayeh' // last name of a person
-let country = 'Finland' // country
-let city = 'Helsinki' // capital city
-let age = 100 // age in years
+
+let firstName = 'Asabeneh'    // first name of a person
+let lastName = 'Yetayeh'      // last name of a person
+let country = 'Finland'       // country
+let city = 'Helsinki'         // capital city
+let age = 100                 // age in years
 let isMarried = true
 
+// const는 재할당이 불가능하므로 바뀔 수 없는 것
 // Declaring variables with number values
-const gravity = 9.81 // earth gravity  in m/s2
-const boilingPoint = 100 // water boiling point, temperature in oC
-const PI = 3.14 // geometrical constant
+
+const gravity = 9.81         // earth gravity  in m/s2
+const boilingPoint = 100     // water boiling point, temperature in oC
+const PI = 3.14              // geometrical constant
 
 // Variables can also be declaring in one line separated by comma
-let name = 'Asabeneh', //name of a person
+
+let name = 'Asabeneh',     //name of a person
   job = 'teacher',
   live = 'Finland'
+
+
+  /* 
+  1. 변수이름은 숫자로 시작하면 안된다.
+  2. 달러 기호와 밑줄을 제외한 특수문자 허용 X
+  3. 이름은 calelCase
+  4. 변수 사이사이 공백이 없어야 한다
+  */
