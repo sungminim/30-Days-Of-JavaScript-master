ad87ca56824a6cb2dab598d71abd11d10a106480
sungminim <__so1@hanmail.net>
2021-04-15 10:32:02 +0900

    day4/21-04-15 finish

--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -1,3 +1,152 @@
 // this is your main.js script
 
-alert('Open the browser console whenever you work on JavaScript')
+/*
+(1)
+let age = Number(prompt('enter your age'));
+
+age >= 18 
+    ? document.write('You are old enough to drive')
+    : document.write(`You are left with ${ 18 - age } years to drive`)
+*/
+
+
+// (2)
+/* let a = 4
+let b = 3
+
+a > b  
+    ? console.log('a is greather than b')
+    : console.log('a is smaller than b')
+*/
+
+// (3)
+/* let num = Number(prompt('숫자를 입력하세요'))
+
+num % 2 === 0 
+    ? alert('짝수다')
+    : alert('홀수다')
+
+console.log(num)
+
+*/
+
+//(3)
+
+/*
+let score = Number(prompt('점수를 입력하세요'))
+
+switch (score){
+    case score >= 80 :
+    alert('A')
+    break;
+
+    case score >= 70 && score < 90 :
+    alert('B')
+    break;
+
+    case score >= 60 && score < 70 :
+    alert('C')
+    break;
+
+    case score >= 50 && score < 60 :
+    alert('D')
+    break;
+
+    default: alert('F')
+}   
+
+*/
+
+// (4)
+/*
+let weather = prompt('What is the day today?')
+
+switch (true){
+    case weather === 'September' :
+    alert('the season is Autumn')
+    break;
+
+    case weather === 'October' :
+    alert('the season is Autumn')
+    break;
+
+    case weather === 'November' :
+    alert('the season is Autumn')
+    break;
+
+    case weather === 'November' :
+    alert('the season is Autumn')
+    break;
+    
+    case weather === 'November' :
+    alert('the season is Autumn')
+    break;
+
+    case weather === 'December' :
+    alert('the season is Winter')
+    break;
+
+    case weather === 'January' :
+    alert('the season is Winter')
+    break;
+
+    case weather === 'February' :
+    alert('the season is Winter')
+    break;
+
+    case weather === 'March':
+    alert('the season is Spring')
+    break;  
+
+    case weather === 'April':
+    alert('the season is Spring')
+    break;  
+
+    case weather === 'May':
+    alert('the season is Spring')
+    break;  
+
+    case weather === 'June':
+    alert('the season is Summer')
+    break;  
+
+    case weather === 'July':
+    alert('the season is Summer')
+    break;  
+
+    case weather === 'August':
+    alert('the season is Summer')
+    break;  
+}   
+*/
+
+// 3
+/*
+let day = prompt('What is the day today?').toLowerCase();
+
+switch (true) {
+    case day === 'monday' :
+    alert('Monday is working day')
+    break;
+    
+    case day === 'tuesday' :
+    alert('Tuesday is working day')
+    break;
+
+    case day === 'wednesday' :
+    alert('Wednesday is working day')
+    break;
+
+    case day === 'firday' :
+    alert('Friday is working day')
+    break;
+
+    case day === 'staurday' :
+    alert('Saturday is holiday')
+    break;
+
+    case day === 'sunday' :
+    alert('Sunday is holiday')
+    break;
+}
+*/
