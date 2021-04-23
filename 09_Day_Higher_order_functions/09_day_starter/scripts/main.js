6df1e1f7efd80732bcb22419399754a3383eb5b7
sungminim <__so1@hanmail.net>
2021-04-23 15:00:59 +0900

    day9/progrmmers

--- 09_Day_Higher_order_functions/09_day_starter/scripts/main.js
+++ 09_Day_Higher_order_functions/09_day_starter/scripts/main.js
@@ -116,8 +116,76 @@
 
 // //  [1,4,9,,16,,25]
 
-const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
-const namesToUpperCase = names.map((name) => name.toUpperCase())
+// const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+// const namesToUpperCase = names.map((name) => name.toUpperCase())
+    
+// console.log(namesToUpperCase)
 
-console.log(namesToUpperCase)
+
+
+// function solution(array, command){
+//     let answer = []
+
+//     command.forEach( command => {
+//         const arr = array.slice(command[0] - 1, command[1]).sort((a , b ) => a - b);
+//         answer.push(arr[command[2]-1]);
+//     });
+
+//     return answer;
+// }
+
+// console.log (solution([1,3,5,2,9], [[2,1,3],[1,2,3]]))
+
+
+/* 
+[알고리즘 1.]
+수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+
+마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+
+*/
+
+// function solution(participant, completion) {
+//     const total = participant.length;
+//     var answer = '';
+    
+//     participant.sort();
+//     completion.sort();
+    
+//     for(let i=0; i<total; i++){
+//         if(participant[i] !== completion[i]){
+//             answer = participant[i];
+//             return answer;
+//         }
+//     }
+// }
+
+// console.log( solution( ['leo', 'kiki', 'eden'], ['eden', 'kiki'] ) )
+
+/* 알고리즘 2 */
+
+// [1,3,5,4,2,1,3,5,4,3,2,1,2,3,1,5,2,1,4,3]
+
+// function solution(answer){
+//     let number = answer.length;
+//     let one = [1, 2, 3, 4, 5];
+
+
+//     for (i=0; i < (number / 5); i++ ){
+//         let newOne = [...one * i]
+//         console.log(newOne)
+//     }
+// }
+
+
+let number = 30;
+let one = [1, 2, 3, 4, 5];
+
+
+for (i=0; i < (number / 5); i++ ){
+    let newOne = [
+        ...one + "," * i
+    ]
+    console.log(newOne)
+}
 
