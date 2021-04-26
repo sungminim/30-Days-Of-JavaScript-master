af6d8b35473c35e6a1ec6b909093f8cc90bb6845
sungminim <__so1@hanmail.net>
2021-04-26 15:35:28 +0900

    day10/ing

--- 10_Day_Sets_and_Maps/10_day_starter/scripts/main.js
+++ 10_Day_Sets_and_Maps/10_day_starter/scripts/main.js
@@ -1,2 +1,38 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// console.log(countries)
+// alert('Open the console and check if the countries has been loaded')
+
+// const languages = [
+//     'English',
+//     'Finnish',
+//     'English',
+//     'French',
+//     'Spanish',
+//     'English',
+//     'French',
+// ]
+
+// const setOfLangauges = new Set(languages)
+// let arr = []
+
+// for (const language of setOfLangauges) {
+// console.log(language)
+// }
+
+
+// let countrie = [
+//     ['Finland', 'Helsinki'],
+//     ['Sweden', 'Stockholm'],
+//     ['Norway', 'Oslo'],
+// ]
+// const map = new Map(countrie)
+// console.log(map)
+// console.log(map.size)
+
+// var mySet = new Set();
+
+// for (i = 1; i<=10;i++){
+//     mySet.add(i)
+// }
+
+// console.log(mySet.delete(8))
+
