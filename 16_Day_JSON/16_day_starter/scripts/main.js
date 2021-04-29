398277629962ee6464529f4ef3df1ceb617d7a28
sungminim <__so1@hanmail.net>
2021-04-29 16:20:04 +0900

    16day/ing

--- 16_Day_JSON/16_day_starter/scripts/main.js
+++ 16_Day_JSON/16_day_starter/scripts/main.js
@@ -1,2 +1,27 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// Exercise
+
+
+const usersText = `{
+    "users":[
+    {
+    "firstName":"Asabeneh",
+    "lastName":"Yetayeh",
+    "age":250,
+    "email":"asab@asb.com"
+    },
+    {
+    "firstName":"Alex",
+    "lastName":"James",
+    "age":25,
+    "email":"alex@alex.com"
+    },
+    {
+    "firstName":"Lidiya",
+    "lastName":"Tekle",
+    "age":28,
+    "email":"lidiya@lidiya.com"
+    }
+]
+}`
+const usersObj = JSON.parse(usersText, undefined, 4)
+console.log(usersObj)
