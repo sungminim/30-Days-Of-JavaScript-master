a042a54e7d6ec7c0d2be4d0bbf47903658b650ad
sungminim <__so1@hanmail.net>
2021-06-11 16:48:01 +0900

    21-06-11

--- 25_Day_World_countries_data_visualization_1/25_day_starter/scripts/main.js
+++ 25_Day_World_countries_data_visualization_1/25_day_starter/scripts/main.js
@@ -1,2 +1,33 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+function populationClick(){
+    let maxVal = countries.sort(function(a,b) {
+        return b.population - a.population;
+    });
+    maxVal = maxVal.slice(0,10)
+    
+    let populationNameArray = maxVal.map((e) => e.name)
+    let populationArray = maxVal.map((e) => e.population)
+    
+    for(i = 0; i<10; i++){
+        document.getElementById("name"+i).innerHTML = populationNameArray[i]
+        document.getElementById("value"+i).innerHTML = populationArray[i].toLocaleString('ko-KR');
+        document.getElementById("bar"+i).innerHTML = (populationArray[i] * 100) / populationArray[0];
+        document.getElementById("bar"+i).style.width = document.getElementById("bar"+i).innerText + "%"
+    }
+}
+
+function languagesClick(){
+    let maxVal = countries.sort(function(a,b) {
+        return b.population - a.population;
+    });
+    maxVal = maxVal.slice(0,10)
+    
+    let populationNameArray = maxVal.map((e) => e.name)
+    let populationArray = maxVal.map((e) => e.population)
+    
+    for(i = 0; i<10; i++){
+        document.getElementById("name"+i).innerHTML = populationNameArray[i]
+        document.getElementById("value"+i).innerHTML = populationArray[i].toLocaleString('ko-KR');
+        document.getElementById("bar"+i).innerHTML = (populationArray[i] * 100) / populationArray[0];
+        document.getElementById("bar"+i).style.width = document.getElementById("bar"+i).innerText + "%"
+    }
+}
